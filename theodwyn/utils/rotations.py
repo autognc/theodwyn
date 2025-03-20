updated guidance logic in collection stack, started eowyn stack, and fixed bugs in control logics and rotation

theodwyn/utils/rotations.py
@@ -4,22 +4,22 @@ from math import cos, sin
 def Rot_1( angle : float ):
     return np.array([
         [ 1., 0.        ,  0.         ],
-        [ 0., cos(angle), -sin(angle) ],
-        [ 0., sin(angle),  cos(angle) ]
+        [ 0., cos(angle),  sin(angle) ],
+        [ 0.,-sin(angle),  cos(angle) ]
     ])
 
 def Rot_2( angle : float ):
     return np.array([
-        [ cos(angle), 0. , sin(angle) ],
+        [ cos(angle), 0. , -sin(angle) ],
         [ 0.        , 1. , 0.         ],
-        [-sin(angle), 0. , cos(angle) ]
+        [ sin(angle), 0. , cos(angle) ]
     ])
 
 def Rot_3( angle : float ):
     return np.array([
-        [ cos(angle), -sin(angle) , 0. ],
-        [ sin(angle),  cos(angle) , 0. ],
-        [ 0.        ,  0.         , 1. ]
+        [ cos(angle), sin(angle) , 0. ],
+        [-sin(angle), cos(angle) , 0. ],
+        [ 0.        , 0.         , 1. ]
     ])
 
 